e2c08ed0ac1579136a25108752e4a3b8fe8ae1e9
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-08 17:52:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :amount, :status
+
+def initialize(sender, receiver, amount)
+ @sender = sender
+ @receiver = receiver
+ @status = "pending"
+ @amount = amount
+end
+
+def valid?
+  @sender.valid? == @receiver.valid?
+end
+
+def execute_transaction
+  
+end
+
+
+
+
+
+
+
+
 end
